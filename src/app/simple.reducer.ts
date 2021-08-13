4b3cc61b89d99eed6d875668274a215760b6cf1e
angeloxenakis <angeloxenakis@gmail.com>
2021-08-12 21:22:06 -0500

    refactored dropdown and added attributes to day

--- src/app/simple.reducer.ts
+++ src/app/simple.reducer.ts
@@ -2,38 +2,26 @@ import { Action } from "@ngrx/store"
 
 export function simpleReducer(
     state: any[] = [
-        {date: 20, high: 94, low: 62},
-        {date: 10, high: 100, low: 63},
-        {date: 13, high: 96, low: 75},
-        {date: 22, high: 92, low: 74},
+        {date: 20, high: 94, low: 62, conditions: "Sunny", rainChance: 20},
+        {date: 10, high: 100, low: 63, conditions: "Sunny", rainChance: 10},
+        {date: 13, high: 96, low: 75, conditions: "Partly Cloudy", rainChance: 40},
+        {date: 22, high: 92, low: 74, conditions: "Rainy", rainChance: 80},
     ], 
     action: Action) {
     console.log(action.type, state)
 
     switch (action.type) {
         case 'DATE':
-            return state = [
-                {date: 10, high: 100, low: 63},
-                {date: 13, high: 96, low: 75},
-                {date: 22, high: 92, low: 74},
-                {date: 20, high: 94, low: 62}
-            ]
+            return state = [...state].sort((a, b) => a.date > b.date ? -1 : 1)
 
         case 'HIGH':
-            return state = [
-                {date: 10, high: 100, low: 63},
-                {date: 13, high: 96, low: 75},
-                {date: 20, high: 94, low: 62},
-                {date: 22, high: 92, low: 74}
-            ]
+            return state = [...state].sort((a, b) => a.high > b.high ? -1 : 1)
 
         case 'LOW':
-            return state = [
-                {date: 13, high: 96, low: 75},
-                {date: 22, high: 92, low: 74},
-                {date: 10, high: 100, low: 63},
-                {date: 20, high: 94, low: 62}
-            ]
+            return state = [...state].sort((a, b) => a.low > b.low ? -1 : 1)
+
+        case 'RAIN':
+            return state = [...state].sort((a, b) => a.rainChance > b.rainChance ? -1 : 1)
         
         default:
             return state
